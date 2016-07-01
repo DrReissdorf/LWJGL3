7535a0ecba9c1c798135d815950dd3f716baf7d8
Sven Riedel <svenmaster89@gmail.com>
2016-07-01 14:10:17 +0200

    zujk

--- src/toolbox/FrameBufferFactory.java
+++ src/toolbox/FrameBufferFactory.java
@@ -130,44 +130,6 @@ public class FrameBufferFactory {
         return gBufferID;
     }
 
-    public static int setupBloomFrameBuffer(Texture hdrTex, Texture brightObjectsTex, int width, int height) {
-        int gBufferID = glGenFramebuffers();
-        glBindFramebuffer(GL_FRAMEBUFFER, gBufferID);
-
-        int attachment1 = GL_COLOR_ATTACHMENT0;
-        int attachment2 = GL_COLOR_ATTACHMENT1;
-
-
-        // - Position color buffer
-        glFramebufferTexture2D(GL_FRAMEBUFFER, attachment1, GL_TEXTURE_2D, hdrTex.getID(), 0);
-
-        // - Normal color buffer
-        glFramebufferTexture2D(GL_FRAMEBUFFER, attachment2, GL_TEXTURE_2D, brightObjectsTex.getID(), 0);
-
-
-        int[] attachments = { attachment1, attachment2 };
-        IntBuffer attribData = BufferUtils.createIntBuffer( attachments.length );
-        attribData.put( attachments, 0, attachments.length );
-        attribData.flip();
-
-        glDrawBuffers(attribData);
-
-        int err = glCheckFramebufferStatus(GL_FRAMEBUFFER);
-        if( err != GL_FRAMEBUFFER_COMPLETE) {
-            System.out.println("Frame buffer is not complete. Error: " + err);
-            System.exit(-1);
-        }
-
-        int depthrenderbuffer = glGenRenderbuffers();
-        glBindRenderbuffer(GL_RENDERBUFFER, depthrenderbuffer);
-        glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, width, height);
-        glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthrenderbuffer);
-
-        glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-        return gBufferID;
-    }
-
     public static int setupShadowFrameBuffer(int shadowTextureID) {
         int shadowFrameBufferID = glGenFramebuffers();
         glBindFramebuffer(GL_FRAMEBUFFER, shadowFrameBufferID);
